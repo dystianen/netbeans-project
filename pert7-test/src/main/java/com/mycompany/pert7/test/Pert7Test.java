47792a409ca95887c3da133b712b35241f20e9ef
Dystian En <dystian.en@kelaspintar.id>
2024-05-31 14:44:02 +0700

    first commit

---
+++ pert7-test/src/main/java/com/mycompany/pert7/test/Pert7Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert7.test;
+
+/**
+ *
+ * @author user.dev
+ */
+public class Pert7Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
